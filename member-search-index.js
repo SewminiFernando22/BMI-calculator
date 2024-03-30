c2c8f732a99e81156f588ad90d8bb138900d846d
Sewmini Fernando <116245302+SewminiFernando22@users.noreply.github.com>
2024-03-30 22:23:47 +0530

    Add files via upload

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"BmiCalculator","l":"BmiCalculator()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"BmiCalculator","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
